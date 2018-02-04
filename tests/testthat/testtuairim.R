fc222a148d6b7b7b0ae5bf35299522ad18cfe419
Caoilfhionn Lane <cldatascience@gmail.com>
2018-02-04 18:35:13 +0000

    remove common list

--- tests/testthat/testtuairim.R
+++ tests/testthat/testtuairim.R
@@ -8,12 +8,13 @@ test_that("test tuairim lexicon", {
         sentences <- read.csv("../testdata/sentences.csv",stringsAsFactors = FALSE)
         
         unigrams <- unnest_tokens(sentences, word, 
-                                  text, token="ngrams", n=1)
+                                  text, token="ngrams", n=1) %>%
+          anti_join(liosta)
         
         sentiment_scores <- unigrams %>%
           left_join(tuairim, by="word") %>%
           group_by(id) %>%
-          mutate(total_words = n())  %>%
+          mutate(total_words = n())  %>% 
           replace_na(replace = list(sentiment="neutral")) %>%
           count(sentiment,id,total_words) %>%
           spread(sentiment, n) %>%
@@ -22,11 +23,11 @@ test_that("test tuairim lexicon", {
           mutate(ratio = 100*((positive - negative)/total_words)) %>%
           left_join(sentences,by="id")
         
-        expect_match(sentiment_scores$ratio[1],20)
-        expect_match(sentiment_scores$ratio[2],25)
-        expect_match(sentiment_scores$ratio[3],-20)
-        expect_match(sentiment_scores$ratio[4],0)
-        expect_match(sentiment_scores$ratio[5],-20)
+        expect_match(sentiment_scores$ratio[1],33.33333)
+        expect_match(sentiment_scores$ratio[2],50)
+        expect_match(sentiment_scores$ratio[3],-50.00000)
+        expect_match(sentiment_scores$ratio[4],-50.00000)
+        expect_match(sentiment_scores$ratio[5],-50)
       }
     }
   }
